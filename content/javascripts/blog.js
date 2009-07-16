9d92ff2ad294b42b18e4245866072b9e8bd5d057
scottmotte <scott@scottmotte.com>
2009-07-16 02:42:34 -0700

    Progress on blog

--- content/javascripts/blog.js
+++ content/javascripts/blog.js
@@ -1,38 +1,17 @@
-// $(document.body).click(function () {
-//    if ($("div:first").is(":hidden")) {
-//      $("div").slideDown("slow");
-//    } else {
-//      $("div").hide();
-//    }
-//  });
-
 $(document).ready(function(){
  // hide articles
  var articles = $('#articles');
  articles.hide();
  
  // toggle articles on click
- $("#show_articles").click(function () {
+ $(".show_articles").click(function () {
    articles.slideToggle();
  });
+ 
+ 	// tweet plugin
+	$(".tweets").tweet({
+		username: "spitfiresky",
+		count: 1,
+		loading_text: "loading tweet"
+	});
 });
-
-
-// // sammy app
-// var app = $.sammy(function() { with(this) {
-//   // Hide articles
-//   $("#articles").hide();
-//   
-//   // debug on
-//   debug = true
-//   
-//   // articles
-//   get('#/articles', function() { with(this) {
-//     console.log('worked!');
-//     $('#articles').slideDown();
-//   }});
-//   
-// }});
-// $(function() {
-//   app.run();
-// });
